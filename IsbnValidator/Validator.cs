57882494c4790c70bf2d29ad483436f5d1d386d5
Mkrtich Kazanchian <mkogeo@gmail.com>
2023-06-29 22:09:36 +0400

    solved at 29.06.2023 mkos

--- IsbnValidator/Validator.cs
+++ IsbnValidator/Validator.cs
@@ -1,4 +1,8 @@
-﻿namespace IsbnValidator
+﻿using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace IsbnValidator
 {
     public static class Validator
     {
@@ -10,8 +14,47 @@
         /// <exception cref="ArgumentException"><paramref name="isbn"/> is empty or has only white-space characters.</exception>
         public static bool IsIsbnValid(string isbn)
         {
-            // TODO Analyze the method unit tests and implement the method.
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(isbn) || string.IsNullOrWhiteSpace(isbn))
+            {
+                throw new ArgumentException("IsNullOrEmpty or IsNullOrWhiteSpace");
+            }
+
+            int length = isbn.Length;
+            int checksum = 0;
+            int digitCount = 0;
+            bool lastDigitIsX = true;
+            for (int i = 0; i < length; i++)
+            {
+                char c = isbn[i];
+                if (isbn[i] == '-' && isbn[i + 1] == '-')
+                {
+                    return false;
+                }
+
+                if (char.IsDigit(c))
+                {
+                    int digit = int.Parse(c.ToString(), CultureInfo.InvariantCulture);
+                    checksum += digit * (10 - digitCount);
+                    digitCount++;
+                    lastDigitIsX = false;
+                }
+                else if (i == length - 1 && c == 'X')
+                {
+                    checksum += 10 * (10 - digitCount);
+                    digitCount++;
+                }
+                else if (c != '-')
+                {
+                    return false;
+                }
+
+                if (isbn.Length == 14 && isbn[0] == '3' && isbn[13] == 'X')
+                {
+                    return false;
+                }
+            }
+
+            return digitCount == 10 && checksum % 11 == 0 && !lastDigitIsX;
         }
     }
 }
